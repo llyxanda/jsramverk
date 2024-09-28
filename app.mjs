6a0fcd8c6e9cecbb22ade9771cc1c080ff7e9c7e
alexandra.berivoe@gmail.com <alexandra.berivoe@gmail.com>
2024-09-28 17:10:25 +0200

    deleted react front end

--- app.mjs
+++ app.mjs
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/posts", posts );
 
+app.get("/", (req, res) => res.send('Hej!') )
 
 if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('combined'));
