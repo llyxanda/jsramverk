34e9aefba3aff82ee6652afd4cd3784456f203cd
alexandra.berivoe@gmail.com <alexandra.berivoe@gmail.com>
2024-10-30 18:21:50 +0100

    Fixed back end tests for graphql authentification

---
+++ test/auth.test.mjs
@@ -0,0 +1,93 @@
+import request from 'supertest';
+import server from '../app.mjs';
+import { expect } from 'chai';
+
+describe('Auth GraphQL API', async () => {
+    it('should register a new user', async () => {
+        const mutation = `
+            mutation {
+                register(email: "testuser@example3.com", password: "testpass") {
+                    message
+                    user {
+                        email
+                    }
+                }
+            }
+        `;
+        const response = await request(server)
+            .post('/graphql/auth')
+            .send({ query: mutation })
+            .set('Accept', 'application/json');
+        
+        console.log('response', response.body);
+
+        expect(response.status).to.equal(200);
+        expect(response.body.data.register).to.be.an('object');
+        expect(response.body.data.register.user).to.be.an('object');
+        expect(response.body.data.register.user.email).to.equal("testuser@example3.com");
+    });
+
+    it('should log in a user', async () => {
+        const mutation = `
+            mutation {
+                login(email: "testuser@example3.com", password: "testpass") {
+                    message
+                    user {
+                        email
+                    }
+                    token
+                }
+            }
+        `;
+        const response = await request(server)
+            .post('/graphql/auth')
+            .send({ query: mutation })
+            .set('Accept', 'application/json');
+        
+        console.log('resp', response.body);
+
+        expect(response.status).to.equal(200);
+        expect(response.body.data.login).to.be.an('object');
+        expect(response.body.data.login.token).to.be.a('string');
+        expect(response.body.data.login.user.email).to.equal("testuser@example3.com");
+    });
+
+    it('should fail to register a user with an existing email', async () => {
+        const mutation = `
+            mutation {
+                register(email: "testuser@example3.com", password: "testpass") {
+                    message
+                }
+            }
+        `;
+        const response = await request(server)
+            .post('/graphql/auth')
+            .send({ query: mutation })
+            .set('Accept', 'application/json');
+        
+        console.log('response hereeee', response.body);
+        expect(response.status).to.equal(200);
+        expect(response.body.data.register.message).to.include('already exists');
+    });
+
+    it('should fail to log in a user with wrong password', async function() {
+        const mutation = `
+            mutation {
+                login(email: "testuser@example3.com", password: "wrongpass") {
+                    message
+                    token
+                }
+            }
+        `;
+        const response = await request(server)
+            .post('/graphql/auth')
+            .send({ query: mutation })
+            .set('Accept', 'application/json');
+        
+        console.log('resppp', response.body);
+        expect(response.status).to.equal(200);
+        expect(response.body.errors).to.be.an('array');
+        expect(response.body.errors[0].message).to.include("Cannot read");
+    });
+});
+
